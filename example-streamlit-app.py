124a2586889cfab55068f45e8221fbacbc9ca3b1
Pavel Hurwicz <pavelhurwicz@gmail.com>
2024-10-20 18:31:10 -0700

    Improved the extraction function in the example app

--- example-streamlit-app.py
+++ example-streamlit-app.py
@@ -8,31 +8,41 @@ st.set_page_config(layout="wide")
 
 @st.cache_data
 def extract_function_definitions(file_content):
-    '''Extract function definitions from a Python file.'''
+    '''
+    Extract function definitions from a Python file.
+    Note that this function does not account for nested definitions.
+    '''
     functions = dict()
     lines = file_content.split('\n')
-    _key, _buffer = None, []
+    _key, _buffer, _line_numbers = None, [], []
     for i, _line in enumerate(lines):
-        _match = re.match(r'def\ ([a-z_]+)\(', _line)
-        if _match:
+        # a function definition starts with a decorator or a def
+        # it also ends any previous definition
+        _deco_match = re.match(r'^@[a-z_]+', _line)
+        _def_match = re.match(r'def\ ([a-z_]+)\(', _line)
+        _unindented_match = re.match(r'^[a-z_]', _line)
+        if _def_match or _deco_match or _unindented_match:
             if _key is not None:
                 _snippet = KeySnippet(
                     key=_key,
                     content='\n'.join(_buffer),
-                    line_numbers=list(range(i - len(_buffer), i)),
+                    line_numbers=_line_numbers,
                 )
                 functions[_key] = _snippet
             _buffer.clear()
-            _key = _match.group(1)
-        elif re.match(r'^[a-z_]', _line):
-            _buffer.clear()
-        _buffer.append(_line)
+            _line_numbers.clear()
+            _key = None
+            if _def_match:
+                _key = _def_match.group(1)
+        if (_deco_match or _def_match) or (not _unindented_match and _key is not None):
+            _buffer.append(_line)
+            _line_numbers.append(i)
     
     if _buffer and _key is not None:
         _snippet = KeySnippet(
             key=_key,
             content='\n'.join(_buffer),
-            line_numbers=list(range(i - len(_buffer), i + 1)),
+            line_numbers=_line_numbers,
         )
         functions[_key] = _snippet
     return functions
